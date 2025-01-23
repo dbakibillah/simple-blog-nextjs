f73c93815420455ac65b62f83e569985a307c6b9
bakibillah <bakibillah.dev@gmail.com>
2025-01-24 03:23:42 +0600

    triying to fix profile

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,15 +1,17 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const ProfilePage = async () => {
-  const session = await getKindeServerSession();
+const ProfilePage = () => {
+  const session = getKindeServerSession();
 
   if (!session || !session.getUser()) {
+    console.error("Redirecting to login: No session or user found.");
     redirect("/api/auth/login");
     return null;
   }
 
   const user = session.getUser();
+  console.log("Authenticated user:", user);
 
   return (
     <div className="container mx-auto mt-10 text-center">
