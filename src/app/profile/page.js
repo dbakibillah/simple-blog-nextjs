e966055c172793f82fd62934190184e6ceb3f7d1
bakibillah <bakibillah.dev@gmail.com>
2025-01-24 03:52:52 +0600

    remove await

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const ProfilePage = async () => {
+const ProfilePage =  () => {
   const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const user =  getUser();
 
   if (!user) {
     redirect("/api/auth/login");
