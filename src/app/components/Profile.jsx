d18b69533ee74eb5f01510401932cb83ce01fcf9
bakibillah <bakibillah.dev@gmail.com>
2025-01-24 14:58:54 +0600

    profile added

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -1,18 +1,21 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const Profile = () => {
-    const { getUser } = getKindeServerSession();
-    const user = getUser();
-console.log(user);
-    if (!user) {
-        redirect("/api/auth/login");
-    }
-    return (
-        <div className="container mx-auto mt-10 text-center">
-            <h2 className="text-2xl font-bold">Welcome to your profile!</h2>
-        </div>
-    );
+const Profile = async () => {
+  const session = getKindeServerSession();
+  const user = await session.getUser();
+
+  if (!user) {
+    redirect("/api/auth/login");
+  }
+
+  return (
+    <div className="container mx-auto mt-10 text-center">
+      <h2 className="text-2xl font-bold">
+        Welcome to your profile, {user.given_name}!
+      </h2>
+    </div>
+  );
 };
 
 export default Profile;
