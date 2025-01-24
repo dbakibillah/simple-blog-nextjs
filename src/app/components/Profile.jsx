2bb4cd4a195d5d26852ad810276e22cfd9e4f67e
bakibillah <bakibillah.dev@gmail.com>
2025-01-24 16:50:41 +0600

    isAuthenticated used

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -2,13 +2,14 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
 const Profile = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const { isAuthenticated } = getKindeServerSession();
+  // const user = await getUser();
 
-  return user ? (
+
+  return isAuthenticated ? (
     <div className="container mx-auto mt-10 text-center">
       <h2 className="text-2xl font-bold">
-        Welcome to your profile, {user.given_name || "Guest"}!
+        Welcome to your profile!
       </h2>
     </div>
   ) : (
